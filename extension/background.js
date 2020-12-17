06d9605b278dcfc48280b1b195306d3f838c0c25
K1DV5 <kidusadugna@gmail.com>
2020-12-17 20:26:20 +0300

    add updating the popup ui

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,37 @@
 uiInfos = {
     1: {
+        state: 0,  // downloading
+        url: 'http://foo',
+        filename: 'foo',
+        size: '23.1MB',
+        speed: '8MB/s',
+        percent: 35,
+        connections: 13,
+        eta: '5m23s',
+        date: '12/16/2020'
+    },
+    2: {
+        state: 1,
+        url: 'http://foo',
+        filename: 'foo',
+        percent: 70,
+        size: '23.1MB',
+        date: '12/16/2020'
+    },
+    3: {
+        state: 2,  // completed
+        url: 'http://foo',
+        filename: 'foo',
+        size: '23.1MB',
+        date: '12/16/2020'
+    }
+}
+
+function addItem(url) {
+    // send to native, then
+    let id = Number((Date.now()).toString().slice(2, -2))
+    let info = {
+        url,
         filename: 'foo',
         size: '23.1MB',
         speed: '8MB/s',
@@ -7,13 +39,10 @@ uiInfos = {
         conns: 'x13',
         eta: '5m23s',
         date: '12/16/2020'
-    },
-    2: {
-        completed: true,
-        filename: 'foo',
-        size: '23.1MB',
-        date: '12/16/2020'
     }
+    uiInfos[id] = info
+    // ? because the popup may be closed now
+    chrome.extension.getViews()[1]?.addRow(info, id)  // popup.addRow
 }
 
 // var port = chrome.runtime.connectNative('com.k1dv5.dman');
--- extension/popup.js
+++ extension/popup.js
@@ -1,5 +1,5 @@
 let bgPage = chrome.extension.getBackgroundPage()
-let listShown = true
+let lastFocusId
 
 const urlInput = document.getElementById('url')
 const list = document.getElementsByTagName('table')[0]
@@ -20,32 +20,36 @@ function addRow(data, id) {
 
     let fnamePart = document.createElement('td')
     row.appendChild(fnamePart)
-    row.id = 'item-' + id
-    if (data.completed) {
-        fnamePart.innerText = data.filename
-    } else {
+    row.id = id
+    if (data.state == 0 || data.state == 1) {
         let fname = document.createElement('div')
         fnamePart.appendChild(fname)
         fname.innerText = data.filename
         let progress = document.createElement('div')
         fnamePart.appendChild(progress)
         progress.className = 'progress'
-        progress.style.width = data.percent
+        progress.style.width = data.percent + '%'
         let info = document.createElement('div')
         fnamePart.appendChild(info)
         info.className = 'info'
-        let percent = document.createElement('span')
-        info.appendChild(percent)
-        percent.innerText = data.percent
-        let speed = document.createElement('span')
-        info.appendChild(speed)
-        speed.innerText = data.speed
-        let conns = document.createElement('span')
-        info.appendChild(conns)
-        conns.innerText = data.conns
-        let eta = document.createElement('span')
-        info.appendChild(eta)
-        eta.innerText = data.eta
+        if (data.state == 0) {
+            let percent = document.createElement('span')
+            info.appendChild(percent)
+            percent.innerText = data.percent + '%'
+            let speed = document.createElement('span')
+            info.appendChild(speed)
+            speed.innerText = data.speed
+            let conns = document.createElement('span')
+            info.appendChild(conns)
+            conns.innerText = 'x' + data.connections
+            let eta = document.createElement('span')
+            info.appendChild(eta)
+            eta.innerText = data.eta
+        } else {
+            info.innerText = 'Rebuilding'
+        }
+    } else {
+        fnamePart.innerText = data.filename
     }
 
     let sizePart = document.createElement('td')
@@ -55,35 +59,44 @@ function addRow(data, id) {
     let datePart = document.createElement('td')
     row.appendChild(datePart)
     datePart.innerText = data.date
+
+    row.addEventListener('focus', () => lastFocusId = id)
 }
 
-let inProgress = []
-for (let [id, info] of Object.entries(bgPage.uiInfos)) {
-    if (info.completed) {
-        addRow(info, id)
-    } else {
-        inProgress.push([id, info])
-    }
-}
-for (let [id, info] of inProgress) {
+for (let [id, info] of Object.entries(bgPage.uiInfos).sort((a, b) => a[1].state < b[1].state ? 1 : -1)) {
     addRow(info, id)
+}
+if (list.rows.length == 1) {
+    let caption = list.createCaption()
+    caption.innerText = 'Downloads appear here.'
+    caption.style.color = '#333'
+}
+
+function update(ids) {
+    for (let id of ids) {
+        let info = bgPage.uiInfos[id]
+        let item = document.getElementById(id)
+        if (info.state == 0) {  // downloading
+            let [_, progress, infoElm] = item.firstElementChild.children
+            progress.style.width = info.percent + '%'
+            let [percent, speed, conns, eta] = infoElm.children
+            percent.innerText = info.percent + '%'
+            speed.innerText = info.speed
+            conns.innerText = 'x' + info.connections
+            eta.innerText = info.eta
+        } else if (info.state == 1) {  // rebuilding
+            let [_, progress, infoElm] = item.firstElementChild.children
+            progress.style.width = info.percent + '%'
+            infoElm.innerHTML = 'Rebuilding'
+        } else {  // completed
+            item.firstElementChild.innerHTML = info.filename
+        }
+    }
 }
 
 function commitUrl() {
-    // send to native, then
-    // let url = urlInput.value
-    let id = Number((Date.now()).toString().slice(2, -2))
-    let info = {
-        filename: 'foo',
-        size: '23.1MB',
-        speed: '8MB/s',
-        percent: '35%',
-        conns: 'x13',
-        eta: '5m23s',
-        date: '12/16/2020'
-    }
-    bgPage.uiInfos[id] = info
-    addRow(info, id)
+    // list.deleteCaption()
+    bgPage.addItem(urlInput.value)
     resetUrl()
 }
 
@@ -97,4 +110,13 @@ document.getElementById('cancel-url').addEventListener('click', resetUrl)
 
 // ===================== LIST ======================
 
+function remove(event) {
+    event.preventDefault()
+    let item = document.getElementById(lastFocusId)
+    if (item == null) return
+    delete bgPage.uiInfos[lastFocusId]
+    item.remove()
+    lastFocusId = undefined
+}
 
+document.getElementById('remove').addEventListener('click', remove)
