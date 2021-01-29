94fbf99142d1bf6ae149e6a3610041c9defd5e20
K1DV5 <kidusadugna@gmail.com>
2021-01-29 19:01:06 +0300

    core: start testing

---
+++ dman/download_test.go
@@ -0,0 +1,46 @@
+// -{go test}
+
+package main
+
+import (
+	"testing"
+	"net/http"
+	"net/url"
+	// "fmt"
+)
+
+
+func TestFilename(t *testing.T) {
+	resp := http.Response{
+		Request: &http.Request{},
+		Header: http.Header{},
+	}
+	for rawurl, fromUrl := range map[string]string{
+			"http://foo.com/bar.zip": "bar.zip",
+			"http://foo.com/bar.zip?foo=bar&baz=bax": "bar.zip",
+			"http://foo.com/foo.boo/bar": "bar",
+		} {
+		URL, _ := url.Parse(rawurl)
+		resp.Request.URL = URL
+		if fname := getFilename(&resp); fname != fromUrl {
+			t.Errorf("Wrong filename from URL: %s != %s", fname, fromUrl)
+		}
+	}
+	resp.Header.Add("Content-Disposition", "attachment; filename=foo.zip")
+	fromHeader := "foo.zip"
+	if fname := getFilename(&resp); fname != fromHeader {
+		t.Errorf("Wrong filename from Header: %s != %s", fname, fromHeader)
+	}
+}
+
+func TestReadableSize(t *testing.T) {
+	for raw, readable := range map[int64]string{
+			1024: "1.00KB",
+			1024 * 1024: "1.00MB",
+			1024 * 1024 * (1024 + 512): "1.50GB",
+		} {
+		if size := readableSize(raw); size != readable {
+			t.Errorf("Wrong readable size: %s != %s", size, readable)
+		}
+	}
+}
